fe46ef8d20ea2190e95560553b8c71aad60e6e5c
Daniel Náger Carpio <dnagerc@inscastellet.cat>
2023-05-22 19:00:50 +0200

    Adding changes

--- App.js
+++ App.js
@@ -1,42 +1,64 @@
-import RegisterScreen from './screens/RegisterScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import NavBar from './screens/NavBar';
-import { StyleSheet } from 'react-native'
+import RegisterScreen from "./screens/RegisterScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import NavBar from "./screens/NavBar";
+import { StyleSheet } from "react-native";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import { initReactI18next } from 'react-i18next';
-import i18n from './lenguages/lenguage';
-import MapScreen from './screens/Profile';
-import UserScreen from './screens/UserScreen';
-import Profile from './screens/Profile';
-import AddParcelScreen from './screens/AddParcelScreen';
-import GetImages from './screens/GetImages'
+import React from "react";
+import { AppRegistry } from "react-native";
+import { initReactI18next } from "react-i18next";
+import i18n from "./lenguages/lenguage";
+import MapScreen from "./screens/Profile";
+import UserScreen from "./screens/UserScreen";
+import Profile from "./screens/Profile";
+import AddParcelScreen from "./screens/AddParcelScreen";
+import GetImages from "./screens/GetImages";
 // Initialize i18next instance with React bindings
 i18n.use(initReactI18next).init();
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  
   return (
     <NavigationContainer style={styles.container}>
-      <Stack.Navigator style={styles.container} initialRouteName="WelcomeScreen">
-        <Stack.Screen style={styles.container} name="NavBar" component={NavBar}  options={{ headerShown: false }}/>
-        <Stack.Screen style={styles.container} name="WelcomeScreen" component={WelcomeScreen}  options={{ headerShown: false }}/>
-        <Stack.Screen style={styles.container} name="Profile" component={Profile}  options={{ headerShown: false }}/>
-        <Stack.Screen style={styles.container} name="RegisterScreen" component={RegisterScreen} options={{ headerShown: true }}/>
+      <Stack.Navigator
+        style={styles.container}
+        initialRouteName="WelcomeScreen"
+      >
+        <Stack.Screen
+          style={styles.container}
+          name="NavBar"
+          component={NavBar}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          style={styles.container}
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          style={styles.container}
+          name="Profile"
+          component={Profile}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          style={styles.container}
+          name="RegisterScreen"
+          component={RegisterScreen}
+          options={{ headerShown: true }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-    );
-
-  
+  );
 }
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white'
-  }
+    backgroundColor: "red",
+    width: "100%",
+    height: "100%",
+  },
 });
--- screens/NavBar.js
+++ screens/NavBar.js
@@ -1,81 +1,98 @@
-import { Text, View ,StyleSheet, Image} from 'react-native'
-import React, { Component , useEffect, useState } from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faMessage, faHome, faSquarePlus, faUser} from '@fortawesome/free-solid-svg-icons';
-import HomeScreenComponent from './HomeScreen';
-import AddParcelScreenComponent from './AddParcelScreen'
-import Profile from './Profile';
-import { useRoute } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-
+import { Text, View, StyleSheet, Image } from "react-native";
+import React, { Component, useEffect, useState } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faMessage,
+  faHome,
+  faSquarePlus,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import HomeScreenComponent from "./HomeScreen";
+import AddParcelScreenComponent from "./AddParcelScreen";
+import Profile from "./Profile";
+import { useRoute } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 
 const HomeScreen = () => {
-  return <HomeScreenComponent />
-}
+  return <HomeScreenComponent />;
+};
 const AddParcelScreen = () => {
-  return <AddParcelScreenComponent/>
-}
+  return <AddParcelScreenComponent />;
+};
 const ChatScreen = () => {
-  return(
-      <View style={styles.container}>
-          <Text style={styles.text}>chat</Text>
-      </View>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>chat</Text>
+    </View>
   );
-}
+};
 
 const UserScreen = ({ user }) => {
-  
   return <Profile user={user} />; //llamar a la pantalla del usuario enviandole el objeto con la info del usuario
 };
 
 const Tab = createBottomTabNavigator();
-function App (){
-
+function App() {
   const route = useRoute();
-  const { user } = route.params 
-  console.log("el user es: "+user.name)
+  const { user } = route.params;
+  console.log("el user es: " + user.name);
 
-    return (
-      <Tab.Navigator>
-      <Tab.Screen 
-        name='h' 
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="h"
         component={HomeScreen}
-        options={{ 
+        options={{
           headerShown: false,
           tabBarShowLabel: false,
           tabBarIcon: ({ color, size }) => (
-            <FontAwesomeIcon icon={faHome} size={30} paddingTop='2%' color="#FF8300" />
+            <FontAwesomeIcon
+              icon={faHome}
+              size={30}
+              paddingTop="2%"
+              color="#FF8300"
+            />
           ),
         }}
       />
-      <Tab.Screen 
-        name='h1' 
+      <Tab.Screen
+        name="h1"
         component={AddParcelScreen}
-        options={{ 
+        options={{
           headerShown: false,
           tabBarShowLabel: false,
           tabBarIcon: ({ color, size }) => (
-            <FontAwesomeIcon icon={faSquarePlus} size={30} paddingTop='2%' color="#FF8300" />
+            <FontAwesomeIcon
+              icon={faSquarePlus}
+              size={30}
+              paddingTop="2%"
+              color="#FF8300"
+            />
           ),
         }}
       />
-      <Tab.Screen 
-        name='h2' 
+      <Tab.Screen
+        name="h2"
         component={ChatScreen}
-        options={{ 
+        options={{
           headerShown: false,
           tabBarShowLabel: false,
           tabBarIcon: ({ color, size }) => (
-            <FontAwesomeIcon icon={faMessage} size={30} paddingTop='2%' color="#FF8300" />
+            <FontAwesomeIcon
+              icon={faMessage}
+              size={30}
+              paddingTop="2%"
+              color="#FF8300"
+            />
           ),
         }}
       />
-      <Tab.Screen 
-        name='h4' 
+      <Tab.Screen
+        name="h4"
         component={UserScreen}
         initialParams={{ user }} //eviar el objeto con la info del usuario a la constante que llama al persil del usuario actual
-        options={{ 
+        options={{
           headerShown: false,
           tabBarShowLabel: false,
           tabBarIcon: ({ color, size }) => (
@@ -83,21 +100,17 @@ function App (){
           ),
         }}
       />
-    </Tab.Navigator>    
-       
-    )
+    </Tab.Navigator>
+  );
 }
 
-
 const styles = StyleSheet.create({
-  container:{
-      flex:1,
-      justifyContent:'center',
-      alignItems:'center',
-      backgroundColor:'red'
-
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
   },
-
 });
 
 export default App;
