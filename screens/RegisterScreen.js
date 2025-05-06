9af469689fe77584f5bef9b959dfdada6aff6e9d
Daniel Nager Carpio <danielnagercarpios@gmail.com>
2025-05-06 21:09:05 +0200

    feat: Improving register page design, aligned agreement text, ordered values

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,5 +1,5 @@
 /*import Checkbox from 'expo-checkbox';*/
-import { Image, Text, View , Button, TextInput, StyleSheet, TouchableOpacity, TouchableHighlight, ScrollView} from 'react-native'
+import { Image, Text, View, Button, TextInput, StyleSheet, TouchableOpacity, TouchableHighlight, ScrollView } from 'react-native'
 import React, { useState } from 'react';
 import { Formik } from 'formik';
 import * as yup from 'yup';
@@ -8,161 +8,146 @@ import { StatusBar } from 'expo-status-bar';
 import { endpoint } from '../interactWithApi/configEndpoints';
 import { useNavigation } from '@react-navigation/native';
 
-
-
-
-
+// Componente de la pagina de registro
 export default function RegisterScreen() {
     const { t } = useTranslation();
 
     const navigation = useNavigation();
 
     const validationSchema = yup.object().shape({
-    email: yup.string().required().email().label("Email"),
-    password: yup.string().min(6, 'Password must have at least 6 characters').required('Password is required'),
-    name: yup.string().max(50, 'Name cant have more than 50 characters').required(t('firtName') + t('requiredMsg')),
-    lastname: yup.string().max(50, 'Lastname cant have more than 50 characters').required(t('lastName') + t('requiredMsg')),
-    day: yup.number().min(1, 'Invalid day').max(31, 'Invalid day').required('Day is required'),
-    month: yup.number().min(1, 'Invalid month').max(12, 'Invalid month').required('Month is required'),
-    year: yup.number().min(1900, 'Invalid year').max(new Date().getFullYear() - 18, 'You must be at least 18 years old').required('Year is required'),
+        email: yup.string().required().email().label("Email"),
+        password: yup.string().min(6, 'Password must have at least 6 characters').required('Password is required'),
+        name: yup.string().max(50, 'Name cant have more than 50 characters').required(t('firtName') + t('requiredMsg')),
+        lastname: yup.string().max(50, 'Lastname cant have more than 50 characters').required(t('lastName') + t('requiredMsg')),
+        day: yup.number().min(1, 'Invalid day').max(31, 'Invalid day').required('Day is required'),
+        month: yup.number().min(1, 'Invalid month').max(12, 'Invalid month').required('Month is required'),
+        year: yup.number().min(1900, 'Invalid year').max(new Date().getFullYear() - 18, 'You must be at least 18 years old').required('Year is required'),
     });
-   
+
     const [toggleCheckBox, setToggleCheckBox] = useState(false);
 
     const handleCheckbox = () => {
-            setToggleCheckBox(!toggleCheckBox)
+        setToggleCheckBox(!toggleCheckBox)
     }
 
-    
-  const registerFunction = (values) => {
-    // todo -> Login
-    console.log(values);
-    fetch( endpoint  +"/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: values.name,
-        password: values.password,
-        last_name: values.lastname,
-        phone_number: "646 198 581",
-        birth_date: "2022-06-02 10:01:10",
-        email: values.email,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response data
-        if (data.id != null) {
-          console.log("EXITO register");
+    const registerFunction = (values) => {
+        //        console.log(values);
+        // Peticiones para el registro
+        fetch(endpoint + "/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: values.name,
+                password: values.password,
+                last_name: values.lastname,
+                phone_number: "646 198 581",
+                birth_date: "2022-06-02 10:01:10",
+                email: values.email,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // Handle the response data
+                if (data.id != null) {
+                    console.log("EXITO register");
 
-          const user = {
-            name: values.name,
-            password: values.password,
-            lastname: values.lastname,
-            email: values.email,
-          } 
+                    const user = {
+                        name: values.name,
+                        password: values.password,
+                        lastname: values.lastname,
+                        email: values.email,
+                    }
 
-          navigation.navigate("NavbarScreen", {user});
-        }
-        console.log(data);
-      })
-      .catch((error) => {
-        // Handle the error
-        console.error(error);
-      });
-  };
+                    navigation.navigate("NavbarScreen", { user });
+                }
+                console.log(data);
+            })
+            .catch((error) => {
+                // Handle the error
+                console.error(error);
+            });
+    };
 
 
+    // Vista
     return (
         <ScrollView style={styles.BigContainer}>
-        <View style={styles.container}>
-            <StatusBar style="auto" /> 
-            { /** LOGO */}
-            <Image style={styles.image} source={require("../assets/BARBACUEUE.png")}  />
+            <View style={styles.container}>
+                <StatusBar style="auto" />
+                { /** LOGO */}
+                <Image style={styles.image} source={require("../assets/BARBACUEUE.png")} />
 
-            { /** FORM */}
-            <Formik
-                initialValues={{ email: '', password: '', name:'', lastname: '', day: '', month: '', year: '' }}
-                onSubmit={(values)=> {
-                    console.log("----> ",values)
-                    registerFunction(values)
-                }
-                }
-                validationSchema={validationSchema}
-                validateOnChange={false} //esto y 
-                validateOnBlur={false} //esto eviata que los errores de validacion aparezcan antes de pulsar el votón Join Us
-            >
-                {({ handleChange, handleSubmit, errors, values }) => (
-                <>
-                    <TextInput keyboardType='email-address' placeholder={t('Email')} style={styles.input} onChangeText={handleChange("email")} value={values.email} />
-                    <Text style={{color:"red"}}>{errors.email}</Text>
-                   
-                    
-                    <TextInput secureTextEntry placeholder={t('Password')} style={styles.input} onChangeText={handleChange("password")} value={values.password} />
-                    <Text style={{color:"red"}}>{errors.password}</Text>
+                { /** FORM */}
+                <Formik
+                    initialValues={{ email: '', password: '', name: '', lastname: '', day: '', month: '', year: '' }}
+                    onSubmit={(values) => {
+                        console.log("----> ", values)
+                        registerFunction(values)
+                    }
+                    }
+                    validationSchema={validationSchema}
+                    validateOnChange={false} //esto y 
+                    validateOnBlur={false} //esto eviata que los errores de validacion aparezcan antes de pulsar el votón Join Us
+                >
+                    {({ handleChange, handleSubmit, errors, values }) => (
+                        <>
+                            <TextInput keyboardType='email-address' placeholder={t('Email')} style={styles.input} onChangeText={handleChange("email")} value={values.email} />
+                            <Text style={{ color: "red" }}>{errors.email}</Text>
 
-                    <TextInput placeholder={t('firtName')} style={styles.input} onChangeText={handleChange("name")} value={values.name} />
-                    <Text style={{color:"red"}}>{errors.name}</Text>
+                            <TextInput placeholder={t('firtName')} style={styles.input} onChangeText={handleChange("name")} value={values.name} />
+                            <Text style={{ color: "red" }}>{errors.name}</Text>
 
-                    <TextInput placeholder={t('lastName')} style={styles.input} onChangeText={handleChange("lastname")} value={values.lastname} />
-                    <Text style={{color:"red"}}>{errors.lastname}</Text>
+                            <TextInput placeholder={t('lastName')} style={styles.input} onChangeText={handleChange("lastname")} value={values.lastname} />
+                            <Text style={{ color: "red" }}>{errors.lastname}</Text>
 
-                    { /** Birthdate fields */}
-                    <Text style={styles.titles}>
-                        {t('bithDate')}
-                    </Text>
+                            <TextInput secureTextEntry placeholder={t('Password')} style={styles.input} onChangeText={handleChange("password")} value={values.password} />
+                            <Text style={{ color: "red" }}>{errors.password}</Text>
 
-                    <View style={styles.birthdateContainer}>
-                        <TextInput       keyboardType="numeric" placeholder="dd" style={[styles.input, styles.specialBirthdateField]} onChangeText={handleChange("day")} value={values.day} />
-                        <TextInput      keyboardType="numeric"  placeholder="mm" style={[styles.input, styles.specialBirthdateField,{ marginStart:'3%'}]} onChangeText={handleChange("month")} value={values.month}/>
-                        <TextInput       keyboardType="numeric" placeholder="yyyy" style={[styles.input, styles.specialBirthdateField,{ marginStart:'3%'}]} onChangeText={handleChange("year")} value={values.year}/>
-                    
-                    
-                    </View>
-                    <View style={styles.inline}>
-                        <TouchableHighlight style={styles.checkBox} onPress={handleCheckbox}>
-                            <Text style={toggleCheckBox ? styles.checks : styles.unChecked}>✔</Text>
-                        </TouchableHighlight>
-                        <Text style={styles.agreement}>{t('agreementContitions')}</Text>               
-                    </View>
-                    <View style={[styles.Button]}>
-                        <Button styles={[styles.Button]} color= 'white' onPress={handleSubmit}  title={t('JoinUsBt')}  accessibilityLabel="Learn more about this purple button"/>
-                    </View>
+                            { /** Birthdate fields */}
+                            {/* <Text style={styles.titles}>
+                                {t('bithDate')}
+                            </Text> 
 
-                </>
-                )}
-        </Formik>
-        </View>
+                            <View style={styles.birthdateContainer}>
+                                <TextInput keyboardType="numeric" placeholder="dd" style={[styles.input, styles.specialBirthdateField]} onChangeText={handleChange("day")} value={values.day} />
+                                <TextInput keyboardType="numeric" placeholder="mm" style={[styles.input, styles.specialBirthdateField, { marginStart: '3%' }]} onChangeText={handleChange("month")} value={values.month} />
+                                <TextInput keyboardType="numeric" placeholder="yyyy" style={[styles.input, styles.specialBirthdateField, { marginStart: '3%' }]} onChangeText={handleChange("year")} value={values.year} />
+                            </View>
+                            */}
+                            <View style={[styles.Button]}>
+                                <Button styles={[styles.Button]} color='white' onPress={handleSubmit} title={t('JoinUsBt')} accessibilityLabel="Learn more about this purple button" />
+                            </View>
+                            <View style={styles.inline}>
+                                <Text style={styles.agreement}>{t('agreementContitions')}</Text>
+                            </View>
+
+                        </>
+                    )}
+                </Formik>
+            </View>
         </ScrollView>
     );
-  
+
 }
 const styles = StyleSheet.create({
     agreement: {
-        paddingHorizontal: 10, // Add ho    
+        paddingHorizontal: 10,
         fontSize: 16,
-        paddingTop: 0,
-        paddingRight: 55,
-        marginBottom: 30,
-        textAlign: 'justify',
-       
-
+        textAlign: 'center',
     },
     inline: {
         flexDirection: 'row', // Set flex direction to row
         alignItems: 'start', // Align items horizontally
-        paddingHorizontal: 42 ,// Add ho   
-        justifyContent:'start',
-        top:'2%'
-      
-      
+        paddingHorizontal: 42,// Add ho   
+        justifyContent: 'start',
+        top: '2%',
     },
     checks: {
         fontSize: 18,
         color: 'blue',
-        
+
     },
     unChecked: {
         fontSize: 18,
@@ -177,45 +162,45 @@ const styles = StyleSheet.create({
         marginLeft: 55,
         position: 'relative',
         display: 'inline',
-        alignItems:'start',
-       
+        alignItems: 'start',
+
     },
-    BigContainer:{
-        backgroundColor:"white",
-        marginTop:"0%"
+    BigContainer: {
+        backgroundColor: "white",
+        marginTop: "0%"
     },
-    container:{
+    container: {
         marginTop: '0%',
         display: 'flex',
         alignItems: 'center',
         backgroundColor: 'white',
-        height:"100%"
+        height: "100%"
     },
     titles: {
         marginTop: 0,
         padding: 10,
-        paddingLeft:0,
-        fontSize: 16 ,
-        width:"70%"
+        paddingLeft: 0,
+        fontSize: 16,
+        width: "70%"
     },
     birthdateContainer: {
         display: "flex",
         flexDirection: "row",
-        width:'70%',
+        width: '70%',
 
     },
     specialBirthdateField: {
         width: "30%",
-        marginStart:0,
-        
-    },
-    detailsConatiner:{
-       
-        padding:20,
+        marginStart: 0,
 
     },
-    input:{
-        width:'70%',
+    detailsConatiner: {
+
+        padding: 20,
+
+    },
+    input: {
+        width: '70%',
         margin: 5,
         // borderWidth: 1,
         padding: 10,
@@ -223,28 +208,28 @@ const styles = StyleSheet.create({
         borderColor: "#CCCCCC",
         color: "#4d4d4d",
         fontSize: 16,
-        
+
     },
-   image:{
-    marginBottom: '8%',
-        width:170,
-        height:130,
+    image: {
+        marginBottom: '8%',
+        width: 170,
+        height: 130,
         alignContent: 'center'
     },
-    title:{
+    title: {
         fontSize: 18,
-        fontWeight:600,
-        
+        fontWeight: 600,
+
     },
-    Button:{
+    Button: {
         fontSize: 40,
-        backgroundColor:'#ff8300',
-        width:'70%',
-        height:60,
-        borderRadius:10,
-        alignItems:'center',
-        justifyContent:'center',
-        marginBottom:"8%",
+        backgroundColor: '#ff8300',
+        width: '70%',
+        height: 60,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: "8%",
         color: "#FFFFFF",
         fontWeight: '120',
         fontSize: 20,
@@ -252,12 +237,12 @@ const styles = StyleSheet.create({
             underlayColor: 'white', // Reset the clicked color effect
         },
     },
-    price:{
-        paddingTop:"2%",
-        paddingBottom:"2%",
-        color:"#F63809",
+    price: {
+        paddingTop: "2%",
+        paddingBottom: "2%",
+        color: "#F63809",
         fontSize: 15,
-        fontWeight:500,        
+        fontWeight: 500,
     },
-   
+
 });
