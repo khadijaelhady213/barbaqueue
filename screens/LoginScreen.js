48996664d81ed23be11776d289ff8992a7b311e1
Daniel Nager Carpio <danielnagercarpios@gmail.com>
2025-05-07 18:01:18 +0200

    feat: Implement Login View structure & add class logo to css styles

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,43 +1,80 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, Button } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { Formik } from 'formik';
+
 
 // Componente de la pagina de Login
 const ViewLoginScreen = () => {
+    const { t } = useTranslation();
+
     return (
         <View style={[
             styles.mainGrid
         ]}>
+            {/* logo + Welcome + Slogan */}
             <View style={[
                 styles.gridItem, styles.red
             ]
             }>
+                <Image
+                    style={styles.logo}
+                    source={require("../assets/BARBACUEUE.png")}
+                />
                 <Text>Welcome to Barbaqueue!</Text>
                 <Text>A Social App</Text>
-
             </View>
-            <View style={[
-                styles.gridItem, styles.green
-            ]
-            }>
-                <Text>Form with email & password & loginbtn & forgot passwordbtn</Text>
-            </View>
-            <View style={[
-                styles.gridItem, styles.blue
-            ]
-            }>
-                <Text>Copyright @DanielNager 2025</Text>
+            {/* Formik Form */}
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+                validate={(values) => {
+                    const errors = {};
+                    if (!values.email) errors.email = 'Email required';
+                    if (!values.password) errors.password = 'Password required';
+                    return errors;
+                }}
+            >
+                {/* Email + Password + Loginbtn */}
+                {({ handleChange, handleSubmit, errors, values }) => (
+                    <>
+                        <View style={[
+                            styles.gridItem, styles.green
+                        ]
+                        }>
+                            <TextInput keyboardType='email-address' placeholder={t('Email')} style={styles.input} onChangeText={handleChange("email")} value={values.email} />
+                            <Text style={{ color: "red" }}>{errors.email}</Text>
 
-            </View>
+                            <TextInput secureTextEntry placeholder={t('Password')} style={styles.input} onChangeText={handleChange("password")} value={values.password} />
+                            <Text style={{ color: "red" }}>{errors.password}</Text>
 
+                            <Button styles={[styles.Button]} color='white' onPress={handleSubmit} title={'forgot password?'} accessibilityLabel="" />
+                        </View>
+                        {/* Register + Copyright */}
+
+                        <View style={[
+                            styles.gridItem, styles.blue
+                        ]
+                        }>
+                            <Button styles={[styles.Button]} color='white' onPress={handleSubmit} title={'Register'} accessibilityLabel="" />
+                            <Text>Copyright @DanielNager 2025</Text>
+
+                        </View>
+                    </>
+                )}
+            </Formik>
         </View>
     )
 }
 
+
 const styles = StyleSheet.create({
     mainGrid: {
         flex: 1,
-        height: "100%",
-        backgroundColor: "red"
+        height: "70%",
+        backgroundColor: "blue"
     },
     gridItem: {
         justifyContent: "center",
@@ -54,6 +91,10 @@ const styles = StyleSheet.create({
     blue: {
         flex: 1,
         backgroundColor: "blue",
+    },
+    logo: {
+        width: 260,
+        height: 200,
     },
 
 
